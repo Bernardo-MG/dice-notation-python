8ad492a2ac4540c18cf855ae44c0f11b0a9fd796
Bernardo Martínez Garrido <programming@wandrell.com>
2016-10-12 15:43:35 +0200

    Added tests for the Dice class

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -16,19 +16,52 @@ __author__ = 'Bernardo Martínez Garrido'
 __license__ = 'MIT'
 
 
-class TestPlaceholder(unittest.TestCase):
+class TestRoll(unittest.TestCase):
     """
-    Placeholder for a test case.
+    Tests the Dice roll method.
     """
 
     def setUp(self):
         """
         Here the tests environment would be prepared.
         """
+        pass
+
+    def test_minimal_rolls1(self):
+        """
+        Tests that the minimal possible dice returns 1.
+        """
         dice = Dice(1, 1)
 
-    def test_true(self):
+        self.assertEqual(1, dice.roll())
+
+    def test_multipleDice_multiplies(self):
         """
-        A placeholder test which is always true.
+        Tests that quantity of dice is multiplied by the sides when rolling.
         """
-        self.assertTrue(True)
+        dice = Dice(3, 1)
+
+        self.assertEqual(3, dice.roll())
+
+    def test_singleDie_range(self):
+        """
+        Tests that the result of rolling a die is kept in the expected range.
+        """
+        dice = Dice(1, 6)
+
+        roll = dice.roll()
+
+        self.assertTrue(roll >= dice.quantity)
+        self.assertTrue(roll <= dice.sides)
+
+    def test_multipleDie_range(self):
+        """
+        Tests that the result of rolling multiple dice is kept in the expected
+        range.
+        """
+        dice = Dice(3, 6)
+
+        roll = dice.roll()
+
+        self.assertTrue(roll >= dice.quantity)
+        self.assertTrue(roll <= (dice.quantity * dice.sides))
