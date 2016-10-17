38a174b931ed377f563f2c0965c8b6d2a23ca3c1
Bernardo Martínez Garrido <programming@wandrell.com>
2016-10-17 22:36:27 +0200

    Added more test cases

--- tests/parser/test_dice_parser_number.py
+++ tests/parser/test_dice_parser_number.py
@@ -51,15 +51,51 @@ class TestNumericBinaryOperation(unittest.TestCase):
 
         self.assertEqual(3, result)
 
-    def test_sub(self):
+    def test_sub_positive(self):
         """
         Tests that numeric subtractions are done correctly.
         """
-        result = self.parser.parse("3-1")
+        result = self.parser.parse("2-1")
 
-        self.assertEqual(2, result)
+        self.assertEqual(1, result)
+
+    def test_sub_negative(self):
+        """
+        Tests that numeric subtractions are done correctly.
+        """
+        result = self.parser.parse("1-2")
+
+        self.assertEqual(-1, result)
+
+    def test_sub_zero(self):
+        """
+        Tests that numeric subtractions are done correctly.
+        """
+        result = self.parser.parse("1-1")
+
+        self.assertEqual(0, result)
+
+
+class TestNumericBinaryOperationLong(unittest.TestCase):
+    """
+    Tests that the parser can work with pure numeric operations.
+    """
+
+    def setUp(self):
+        """
+        Here the tests environment would be prepared.
+        """
+        self.parser = DiceParser()
 
     def test_longAdd(self):
+        """
+        Tests that numeric additions are done correctly.
+        """
+        result = self.parser.parse("1+2+3")
+
+        self.assertEqual(6, result)
+
+    def test_longerAdd(self):
         """
         Tests that numeric additions are done correctly.
         """
@@ -71,9 +107,29 @@ class TestNumericBinaryOperation(unittest.TestCase):
         """
         Tests that numeric additions are done correctly.
         """
+        result = self.parser.parse("1-2-3")
+
+        self.assertEqual(-4, result)
+
+    def test_longerSub(self):
+        """
+        Tests that numeric additions are done correctly.
+        """
         result = self.parser.parse("1-2-3-4-5")
 
         self.assertEqual(-13, result)
+
+
+class TestNumericBinaryOperationMixed(unittest.TestCase):
+    """
+    Tests that the parser can work with pure numeric operations.
+    """
+
+    def setUp(self):
+        """
+        Here the tests environment would be prepared.
+        """
+        self.parser = DiceParser()
 
     def test_addAndSub(self):
         """
