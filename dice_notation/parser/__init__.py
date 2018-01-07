6f5ea67409079544cd4ff6557ed6a5bd75c746aa
Bernardo <programming@wandrell.com>
2018-01-07 22:17:32 +0100

    Using library for reading the version

--- dice_notation/parser/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from dice_notation.parser.dice import DiceParser
-
-__all__ = ['DiceParser']
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from codecs import open
 
 from setuptools import find_packages, setup
 from bernardomg.tox_test_command import ToxTestCommand
+from version_extractor import extract_version_init
 
 """
 PyPI configuration module.
@@ -31,26 +32,13 @@ def read(*names, **kwargs):
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
-
-# Gets the version for the source folder __init__.py file
-def read_version(path):
-    # Regular expression for the version
-    _version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-    with open(path + '__init__.py', 'rb', encoding='utf-8') as f:
-        version_lib = f.read()
-        version_lib = _version_re.search(version_lib).group(1)
-        version_lib = str(ast.literal_eval(version_lib.rstrip()))
-
-    return version_lib
-
 setup(
     name='dice-notation',
     packages=find_packages(),
     include_package_data=True,
     package_data={
     },
-    version=read_version(_source_package),
+    version=extract_version_init(_source_package),
     description='Dice notation tools',
     author='Bernardo Martínez Garrido',
     author_email='programming@bernardomg.com',
