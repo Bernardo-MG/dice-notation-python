cbc04c79cf419a4ad69fd4f29400dbe5bebba67b
Bernardo <programming@wandrell.com>
2018-01-07 13:23:40 +0100

    The test command is now taken as a dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os.path import join
 from codecs import open
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as test_command
+from bernardomg.tox_test_command import ToxTestCommand
 
 """
 PyPI configuration module.
@@ -41,41 +41,6 @@ with open(_source_package + '/__init__.py', 'rb', encoding='utf-8') as f:
     version_lib = f.read()
     version_lib = _version_re.search(version_lib).group(1)
     version_lib = str(ast.literal_eval(version_lib.rstrip()))
-
-
-class _ToxTester(test_command):
-    """
-    Tox test command.
-
-    Calls tox for running the tests.
-    """
-    user_options = [
-        ('test-module=', 'm', "Run 'test_suite' in specified module"),
-        ('test-suite=', 's',
-         "Run single test, case or suite (e.g. 'module.test_suite')"),
-        ('test-runner=', 'r', "Test runner to use"),
-        ('profile=', 'p', 'Test profile to use')
-    ]
-
-    def initialize_options(self):
-        test_command.initialize_options(self)
-        self.profile = None
-
-    def finalize_options(self):
-        test_command.finalize_options(self)
-        self.test_args = []
-
-        if self.profile is not None:
-            # Adds the profile argument
-            # For example: '-e=py36'
-            self.test_args.append('-e=' + self.profile)
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-
-        errcode = tox.cmdline(self.test_args)
-        sys.exit(errcode)
 
 
 setup(
@@ -111,6 +76,6 @@ setup(
     tests_require=_tests_require,
     extras_require={'test': _tests_require},
     cmdclass={
-        'test': _ToxTester
+        'test': ToxTestCommand
     },
 )
