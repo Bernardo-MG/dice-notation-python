c0a66d05d9775aba43595afd1aaa77f0ed7fdce3
Bernardo <programming@wandrell.com>
2020-03-08 18:56:20 +0100

    Added ANTLR grammar

---
+++ dice_notation/parser/DiceNotationListener.py
@@ -0,0 +1,60 @@
+# Generated from DiceNotation.g4 by ANTLR 4.7.2
+from antlr4 import *
+
+# This class defines a complete listener for a parse tree produced by DiceNotationParser.
+class DiceNotationListener(ParseTreeListener):
+
+    # Enter a parse tree produced by DiceNotationParser#notation.
+    def enterNotation(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#notation.
+    def exitNotation(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DiceNotationParser#addOp.
+    def enterAddOp(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#addOp.
+    def exitAddOp(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DiceNotationParser#multOp.
+    def enterMultOp(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#multOp.
+    def exitMultOp(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DiceNotationParser#operand.
+    def enterOperand(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#operand.
+    def exitOperand(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DiceNotationParser#dice.
+    def enterDice(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#dice.
+    def exitDice(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DiceNotationParser#number.
+    def enterNumber(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DiceNotationParser#number.
+    def exitNumber(self, ctx):
+        pass
+
+
