53958574e8543d742ef864a2f4a940e398cba3de
Bernardo <programming@wandrell.com>
2020-03-08 19:31:06 +0100

    Corrections to parser

--- dice_notation/parser/dice.py
+++ dice_notation/parser/dice.py
@@ -1,5 +1,9 @@
-import sys
+import io
 import logging
+from antlr4 import InputStream, CommonTokenStream
+
+from dice_notation.parser.DiceNotationLexer import DiceNotationLexer
+from dice_notation.parser.DiceNotationParser import DiceNotationParser
 
 
 class DiceParser():
@@ -7,3 +11,10 @@ class DiceParser():
     def __init__(self):
         super(DiceParser, self).__init__()
         self._logger = logging.getLogger("DiceParser")
+
+    def parse(self, input):
+        input_stream = InputStream(input + "\n")
+        lexer = DiceNotationLexer(input_stream)
+        stream = CommonTokenStream(lexer)
+        parser = DiceNotationParser(stream)
+        return parser.notation()
